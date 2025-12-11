9c3ab14219f137b2d70d67b706ce1aefb518c4e4
DEHER Rémi <56632525+remi-deher@users.noreply.github.com>
2025-12-11 17:50:48 +0100

    Remove comment about Composer autoloader

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 session_start();
 
-// Inclut l'autoloader de Composer (si vous l'utilisez)
+// Inclut l'autoloader de Composer
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
 }
